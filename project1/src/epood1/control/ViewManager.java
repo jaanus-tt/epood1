dc01e499a650aedf360b6e6159b66ca4f1a4e6cc
kristerlukka <kristerlukka@laptop592.webmedia.int>
2013-11-12 19:22:59 +0200

    aaa

--- project1/src/epood1/control/ViewManager.java
+++ project1/src/epood1/control/ViewManager.java
@@ -15,31 +15,31 @@ import epood1.log.MyLogger;
  *   May 16, 2013
  */
 public class ViewManager {
-	
-	public static void navigate(String view, HttpServletRequest request, HttpServletResponse response,ServletContext context) 
-				throws ServletException, IOException  {
 
-        try {
-        	
-        	if (view.equals("catalog")) {
-        		context.getRequestDispatcher("/jsp/products.jsp").forward(request, response);
-           	} else if (view.equals("product")){
-        		context.getRequestDispatcher("/jsp/product.jsp").forward(request, response);
-        	} else if (view.equals("cart")){
-        		context.getRequestDispatcher("/jsp/cartItems.jsp").forward(request, response);
-        	} else if (view.equals("searchOrder")){
-        		context.getRequestDispatcher("/jsp/orderHistory.jsp").forward(request, response);
-        	} else if (view.equals("orderItems")){
-        		context.getRequestDispatcher("/jsp/orderItems.jsp").forward(request, response);
-        	} else if (view.equals("searchProductsForm")){
-        		context.getRequestDispatcher("/jsp/productSearchForm.jsp").forward(request, response);
-        	}
-        	
-        } catch (Exception ex) {
+	public static void navigate(String view, HttpServletRequest request, HttpServletResponse response,ServletContext context) 
+			throws ServletException, IOException  {
+
+		try {
+
+			if (view.equals("catalog")) {
+				context.getRequestDispatcher("/jsp/products.jsp").forward(request, response);
+			} else if (view.equals("product")){
+				context.getRequestDispatcher("/jsp/product.jsp").forward(request, response);
+			} else if (view.equals("cart")){
+				context.getRequestDispatcher("/jsp/cartItems.jsp").forward(request, response);
+			} else if (view.equals("searchOrder")){
+				context.getRequestDispatcher("/jsp/orderHistory.jsp").forward(request, response);
+			} else if (view.equals("orderItems")){
+				context.getRequestDispatcher("/jsp/orderItems.jsp").forward(request, response);
+			} else if (view.equals("searchProductsForm")){
+				context.getRequestDispatcher("/jsp/productSearchForm.jsp").forward(request, response);
+			}
+
+		} catch (Exception ex) {
 			MyLogger log = new MyLogger();
 			log.Log("ViewManager.navigate():",ex.getMessage());
-        }
+		}
 
-    }
+	}
 
 }
